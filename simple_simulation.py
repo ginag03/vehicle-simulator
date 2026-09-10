c448152a87d49172f3ab63e134c610605edaf403
Gina Gundersen <ginagundersen03@gmail.com>
2026-09-10 08:11:36 +0100

    feat: implement basic kinematic bicycle model

---
+++ simple_simulation.py
@@ -0,0 +1,35 @@
+import numpy as np
+import plotly.express as px
+
+# basic parameters
+L = 2.5 # wheelbase (m)
+v = 5 # speed (m/s)
+delta = 0.17 # steering angle (radians)
+dt = 0.01 # timestep (s)
+total_sim_time = 60
+
+steps = int(total_sim_time / dt)
+
+x = [0]
+y = [0]
+psi = [0]
+
+n = 0
+
+for n in range(steps):
+    x.append(x[n] + v * np.cos(psi[n]) * dt)
+    y.append(y[n] + v * np.sin(psi[n]) * dt)
+    psi.append(psi[n] + (v / L) * np.tan(delta) * dt)
+
+print("x values:")
+print(x[:10])
+print("y values:")
+print(y[:10])
+print("psi values:")
+print(psi[:10])
+print("Final position: x = {:.2f}, y = {:.2f}, psi = {:.2f}".format(x[-1], y[-1], psi[-1]))
+
+fig = px.line(x=x, y=y)
+fig.update_layout(template="plotly_white")
+fig.update_yaxes(scaleanchor="x", scaleratio=1)
+fig.show()
